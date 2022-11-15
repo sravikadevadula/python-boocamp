f36ceb349eecd0f8a303819545d20ef3b96700ed
sravikadevadula <sreedevi@1994>
2022-11-15 10:27:25 +0530

    friday week2

--- github.py
+++ github.py
@@ -1,1 +1,18 @@
-print('my github')
+def input_list():
+    numbers = None
+    l = []
+    while numbers != '':
+        numbers = input('enter the number:')
+        l.append(numbers)
+    return l
+def final_list():
+    l1 = input_list()
+    l1.pop(-1)
+    total = 0.0
+    for i in l1:
+        total = total + float(i)
+    l1.append(total)
+    print(l1)
+
+
+final_list()
--- practice course/pratice 2.py
+++ practice course/pratice 2.py
@@ -1,26 +1,42 @@
-import turtle
-
-display=turtle.Screen()
-turtle = turtle.Turtle()
-turtle.color("orange")
-turtle.pensize(8)
-turtle.shape("arrow")
-
-def triangle(edge=100):
-    turtle.forward(edge)
-    turtle.right(120)
-    turtle.forward(edge)
-    turtle.right(120)
-    turtle.forward(edge)
-    turtle.right(120)
-    turtle.forward(edge)
-    turtle.right(60)
-
-triangle()
-triangle()
-triangle()
-triangle()
-triangle()
-triangle()
-
-display.mainloop()
+# import turtle
+#
+# display=turtle.Screen()
+# turtle = turtle.Turtle()
+# turtle.color("orange")
+# turtle.pensize(8)
+# turtle.shape("arrow")
+#
+# def triangle(edge=100):
+#     turtle.forward(edge)
+#     turtle.right(120)
+#     turtle.forward(edge)
+#     turtle.right(120)
+#     turtle.forward(edge)
+#     turtle.right(120)
+#     turtle.forward(edge)
+#     turtle.right(60)
+#
+# triangle()
+# triangle()
+# triangle()
+# triangle()
+# triangle()
+# triangle()
+#
+# display.mainloop()
+# import numpy as np
+# lis=[]
+# sequence=[1,2,2,3]
+# arr = np.array(sequence)
+# a=np.sort(arr)
+# b=a[::-1]
+# #
+# # np.array[::-1].sort(sequence)
+# lis.append((a == arr).any())
+# # lis.append((b==arr).any())
+# print(lis)
+# # print(b)
+import random
+word='apple'
+# b=random.randrange(0,len(word))
+print(word[random.randrange(0,len(word))])
